54ca4cd5326796891e4ec7c2e270bd36d75770c2
bz888 <ziyu.ben158@gmail.com>
2021-10-21 21:53:59 +1300

    fixed test routes

--- routes.js
+++ routes.js
@@ -2,21 +2,66 @@ const express = require("express");
 
 const router = express.Router();
 
-const { getData } = require("./utils");
+const { getData, updateData } = require("./utils");
 
 module.exports = router;
 
 router.get("/:movies", (req, res) => {
-  const req_Title = req.params.movies;
   getData((err, returnData) => {
     if (err) {
       res.status(500).send(err.message);
       return;
     }
-    const moviesData = {
-      movies: returnData.movies,
-    };
+    const req_Title = req.params.movies;
+
     const returnTitle = returnData.movies.find((el) => el.title === req_Title);
     res.render("test", returnTitle);
   });
 });
+
+router.get('/:movies/edit', (req, res) =>{
+    getData((err, returnData) =>{
+        if (err) {
+            res.status(500).send(err.message)
+            return
+        }
+        const req_Title = req.params.movies;
+        const returnTitle = returnData.movies.find((el) => el.title === req_Title);
+        res.render("edit", returnTitle);
+      })
+      
+    })
+    
+    router.post('/:movies/edit', (req, res) => {
+      getData((err,data) =>{
+        if(err){
+          res.status(500).send(err.message)
+          return
+        }
+        const req_Title = req.params.movies;
+        const dataArrTest = data.movies
+        const returnTitle = dataArrTest.find((el) => el.title === req_Title);
+
+        const updateReview = {
+            ...returnTitle,
+            // id: parseInt(req.params.movies),
+            // title: req.body.movies,
+            review: req.body.review
+        }
+        console.log(updateReview);
+
+        const filtered = dataArrTest.filter(element => element.title !== req.params.movies ) 
+        const newArr = [...filtered, updateReview]
+        const newData = {movies: newArr}
+        
+        updateData(newData, (err) => {
+          if (err) {
+            res.status(500).send(err.message)
+            return
+          }
+          // console.log(updateReview);
+          res.redirect('/movies/' + returnTitle.title)
+        })
+
+    })
+})
--- utils.js
+++ utils.js
@@ -2,23 +2,41 @@ const fs = require("fs");
 const path = require("path");
 
 module.exports = {
-  getData,
+  getData, updateData
 };
 
 function getData(cb) {
   const filepath = path.join(__dirname, "test.json");
   fs.readFile(filepath, "utf8", (err, contents) => {
-    if (err) {
-      console.error(err.message);
-      cb(new Error("Unable to load the file"));
-      return;
-    }
+      if (err) {
+          console.error(err.message);
+          cb(new Error("Unable to load the file"));
+          return;
+        }
+        try {
+            const parseData = JSON.parse(contents);
+            cb(null, parseData);
+        } catch (parseErr) {
+            console.error(parseErr);
+            cb(new Error("Unable to parse the data file"));
+        }
+    });
+}
+
+function updateData (newData, cb){
     try {
-      const parseData = JSON.parse(contents);
-      cb(null, parseData);
-    } catch (parseErr) {
-      console.error(err.message);
-      cb(new Error("Unable to parse the data file"));
+        const stringData = JSON.stringify(newData, null, 2)
+        const filepath = path.join(__dirname, "test.json");
+        
+        fs.writeFile(filepath, stringData, 'utf-8', (err) =>{
+            if (err) {
+                console.error(err.message)
+                cb(new Error('oh no, its broken'))
+            }
+            cb()
+        })
+    } catch (err) {
+        console.error(err.message)
+        cb(new Error('oh no it didn\'t change'))
     }
-  });
 }
